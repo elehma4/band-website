6d56cf049997bd3f8361b17511f7d25f343b07e5
ETH1Elohim <ezmax.defi@gmail.com>
2023-05-03 13:38:03 -0400

    albums page is finished

--- routes/albums.js
+++ routes/albums.js
@@ -4,11 +4,17 @@ const router = express.Router() //sub routing
 
 //import data from data.json
 
-// const data = require('../data/data.json');
+const data = require('../data/data.json');
+
+// console.log(data.albums); 
+
+let albums = data.albums; // arr of objs
 
 router.get('/albums', (req, res)=>{
 
-    res.render('albums')
+    res.render('albums', {
+        albums: albums
+    })
 })
 
 module.exports = router;
